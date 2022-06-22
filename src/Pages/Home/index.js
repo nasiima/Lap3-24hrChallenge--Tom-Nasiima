2561794f67fe8ee44af3afcd4158e23ea11f20bd
nasiima <nasiima.mohamed1@gmail.com>
2022-06-22 11:48:39 +0100

    creating funcs for form submit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
 
+import './style.css'
+
 function Home(){
     const [username, setUsername] = useState('')
 
 
 
+    const displayRepo = () => {}
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+      };
+    
+      const updateInput = (e) => {
+        const input = e.target.value;
+        setUsername(input);
+      };
+
+
 
     return (
         <>
-         <form role="form">
-          <label htmlFor="usernameSearch">Github Username: </label>
+         <form className="container" role="form" onSubmit={handleSubmit}>
+          <label htmlFor="usernameSearch" >Github Username: </label>
           <input
+            id="usernameSearch"
             role="username"
             type="text"
+            className="userNameSearch"
+            onChange={updateInput}
           />
-          <input className="button" type="submit" value="Search"  />
+          <input className="button" type="submit" value="Search" onClick={displayRepo} />
         </form>
-        
-        
         </>
 
 
