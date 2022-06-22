c2941a6f3ed3af1c4f35d1549dc3ae41019bf3db
nasiima <nasiima.mohamed1@gmail.com>
2022-06-22 11:34:55 +0100

    creating controlled form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import {Home} from './Pages'
 
 
 
 function App() {
   return (
     <div className="App">
-
-   
+       <Home />
     </div>
   );
 }
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,1 +1,29 @@
-import React from react
+import React, { useState } from 'react';
+
+function Home(){
+    const [username, setUsername] = useState('')
+
+
+
+
+    return (
+        <>
+         <form role="form">
+          <label htmlFor="usernameSearch">Github Username: </label>
+          <input
+            role="username"
+            type="text"
+          />
+          <input className="button" type="submit" value="Search"  />
+        </form>
+        
+        
+        </>
+
+
+    )
+
+
+}
+
+export default Home
