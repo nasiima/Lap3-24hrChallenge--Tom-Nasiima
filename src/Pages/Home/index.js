0067be07b16d05cd4449348ad301e4f7f100f5bd
nasiima <nasiima.mohamed1@gmail.com>
2022-06-24 12:22:57 +0100

    styling

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -22,44 +22,44 @@ function Home(){
         async function fetchData() {
             try {
                 
-                let issueCounter = 0
-                let folkCounter = 0
-                let sizeCounter = 0
-                let watchCounter = 0
-                let isMainCounter = 0
-                let reposCounter = 0
+                // let issueCounter = 0
+                // let folkCounter = 0
+                // let sizeCounter = 0
+                // let watchCounter = 0
+                // let isMainCounter = 0
+                // let reposCounter = 0
             
             const git = await axios.get(`https://api.github.com/users/${username}/repos`);
             console.log(git.data);
             setRepos(git.data);
 
-            reposCounter = git.data.length
+            // reposCounter = git.data.length
 
-            for (let index = 0; index < git.data.length; index++) {
+            // for (let index = 0; index < git.data.length; index++) {
                 
-                const issues = git.data[index].open_issues;
-                const folks = git.data[index].folk_counts;
-                const sizes = git.data[index].size;
-                const watchers = git.data[index].watchers_count;
-                const isMain = (git.data[index].default_branch === "main") ? 1 : 0; 
+            //     const issues = git.data[index].open_issues;
+            //     const folks = git.data[index].folk_counts;
+            //     const sizes = git.data[index].size;
+            //     const watchers = git.data[index].watchers_count;
+            //     const isMain = (git.data[index].default_branch === "main") ? 1 : 0; 
                 
                 
 
-                issueCounter +=issues;
-                folkCounter += folks
-                sizeCounter += sizes
-                watchCounter += watchers
-                isMainCounter += isMain
-            }
+            //     issueCounter +=issues;
+            //     folkCounter += folks
+            //     sizeCounter += sizes
+            //     watchCounter += watchers
+            //     isMainCounter += isMain
+            // }
 
-            console.log("TOTAL ISSUES",issueCounter)
-            console.log("TOTAL FOLKS",folkCounter)
-            console.log("TOTAL PUBLIC REPOS",reposCounter);
-            console.log("TOTAL SIZE (kb)",sizeCounter);
-            console.log("TOTAL WATCHERS",watchCounter);
-            console.log("TOTAL 'main' BRANCHS",isMainCounter);
+            // console.log("TOTAL ISSUES",issueCounter)
+            // console.log("TOTAL FOLKS",folkCounter)
+            // console.log("TOTAL PUBLIC REPOS",reposCounter);
+            // console.log("TOTAL SIZE (kb)",sizeCounter);
+            // console.log("TOTAL WATCHERS",watchCounter);
+            // console.log("TOTAL 'main' BRANCHS",isMainCounter);
 
-            navigate("/repolist");
+            // navigate("/repolist");
         
             // navigate("/repolist",{issueCounter} );
 
